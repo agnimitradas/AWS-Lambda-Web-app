b2cfaceb3ed55658597295c16ec940dcec618082
Agnimitra Das <AD00440946@techmahindra.com>
2017-05-09 15:46:47 +0530

    fc

--- src/main/java/com/bank/chaitu/customer/SavingAccount.java
+++
@@ -1,5 +0,0 @@
-package com.bank.chaitu.customer;
-
-public class SavingAccount {
-
-}
--- src/main/java/com/start/springboot/StartUIApplication.java
+++ src/main/java/com/start/springboot/StartUIApplication.java
@@ -1,31 +1,20 @@
 package com.start.springboot;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootConfiguration
-@ComponentScan
 @EnableAutoConfiguration
-@ComponentScan({"com.chaitu.swiggy.service","com.chaitu.employee","com.chaitu.hello.controller,com.samples.customer.mongoDB,com.sample.projectModule,com.mysql.hibernate"})
-public class StartBootApplication {
+@CrossOrigin
+public class StartUIApplication {
 
-    public static void main(String[] args) {
-   
-      ApplicationContext app = SpringApplication.run(StartBootApplication.class, args);
-        
-       System.out.println("Let's inspect the beans provided by Spring Boot:");
+	public static void main(String[] args) {
 
-      String[] beanNames = app.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
-   
-  
+		SpringApplication.run(StartUIApplication.class, args);
+
+		System.out.println("Spring Boot Aws Demo Appkication Started...");
+
+	}
 }
